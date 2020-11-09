34d6c08ab0781100af320d77ebbee0a814e9686f
slovty <1403528766@qq.com>
2020-11-09 17:02:58 +0800

    feat(add docker ): add docker

--- simple_demo/ginapi.go
+++ simple_demo/ginapi.go
@@ -15,7 +15,9 @@ func main() {
 	m1 := make(map[string]map[string]string)
 	m1["data_obj"] = make(map[string]string)
 	m1["data_obj"]["id"] = "2"
-	m1["data_obj"]["name"] = "史纯涛"
+	datetime := "2020"
+	m1["data_obj"]["name"] = datetime
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"code": 200,
