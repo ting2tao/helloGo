efa09e75a8c4f575c5c578abfda4f15bfe23c9fd
史纯涛 <1403528766@qq.com>
2020-11-27 16:49:08 +0800

    update gitignore

---
+++ simple_demo/convert.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var ut uint = 89
+	fmt.Println(ut)
+	fmt.Println(strconv.Itoa(int(ut)))
+}
--- simple_demo/hel.go
+++ simple_demo/hel.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("66666")
+	fmt.Println("345345")
 }
