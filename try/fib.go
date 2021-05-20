7dc61238cf0c3ed3dfa018f578de7f4207f14c85
史纯涛 <1403528766@qq.com>
2021-05-20 18:57:00 +0800

    feat(new): add fib

--- try/fib.go
+++ try/fib.go
@@ -24,3 +24,17 @@ func helper(m map[int]int, n int) int {
 	m[n] = helper(m, n-1) + helper(m, n-2)
 	return m[n]
 }
+
+// 自下而上的dp 数组的迭代解法
+func dpHelper(n int) int {
+	if n < 2 {
+		return n
+	}
+	dp := []int{}
+	dp[1] = 1
+	dp[2] = 1
+	for i := 3; i <= n; n++ {
+		dp[n] = dp[n-1] + dp[n-2]
+	}
+	return dp[n]
+}
