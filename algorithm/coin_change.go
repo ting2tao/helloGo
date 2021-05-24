fccf447c21c88c9a7b35382d317deb460d0383c0
史纯涛 <1403528766@qq.com>
2021-05-24 10:29:32 +0800

    feat(new): add coinChange

---
+++ algorithm/coin_change.go
@@ -0,0 +1,10 @@
+package algorithm
+
+func main() {
+
+}
+
+// 暂领取
+func coinChange(coins []int, amount int) int {
+	return 0
+}
---
+++ try/arrslice2.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func main() {
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+	fmt.Println(sqrt(2), sqrt(-4))
+}
+
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
