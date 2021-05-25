135b59ffe352d55e9a68b716292947c92c3d6bf5
shichuntao <1403528766@qq.com>
2021-05-25 20:57:05 +0800

    feat(new):add s.th

---
+++ algorithm/coin_change.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(coinChange([]int{1, 2, 5}, 11))
+}
+
+func coinChange(coins []int, amount int) int {
+
+	return 0
+}
--- try/fib.go
+++ try/fib.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println(fib(30))
+	fmt.Println(dpHelper(30))
+	fmt.Println(niceDpHelper(30))
+	fmt.Println(leeCodeFib(30))
 }
 func fib(n int) int {
 	if n < 2 {
@@ -30,11 +33,41 @@ func dpHelper(n int) int {
 	if n < 2 {
 		return n
 	}
-	dp := []int{}
+	dp := make([]int, n+1)
+	dp[0] = 0
 	dp[1] = 1
 	dp[2] = 1
-	for i := 3; i <= n; n++ {
-		dp[n] = dp[n-1] + dp[n-2]
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
 	}
 	return dp[n]
+}
+
+// 自下而上的dp 数组的迭代解法 优化 状态压缩 使得空间复杂度
+// 一般来说是把一个二维的 DP table 压缩成一维，即把空间复杂度从 O(n^2) 压缩到 O(n)
+func niceDpHelper(n int) int {
+	if n < 2 {
+		return n
+	}
+	prev := 1
+	curr := 1
+	for i := 3; i <= n; i++ {
+		sum := prev + curr
+		prev = curr
+		curr = sum
+	}
+	return curr
+}
+
+func leeCodeFib(n int) int {
+	if n < 2 {
+		return n
+	}
+	p, q, r := 0, 0, 1
+	for i := 2; i <= n; i++ {
+		p = q
+		q = r
+		r = p + q
+	}
+	return r
 }
