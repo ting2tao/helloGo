de52a8439f264b2ac312f84a0895334b74a04ad3
shichuntao <1403528766@qq.com>
2021-05-26 21:39:23 +0800

    feat(new):add func

--- simple_demo/func.go
+++ simple_demo/func.go
@@ -19,6 +19,9 @@ func main() {
 	fmt.Println(compute(math.Pow))
 	fib1()
 
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Abs())
 }
 
 // 返回一个“返回int的函数”
@@ -40,4 +43,17 @@ func fib1() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(f(i))
 	}
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
 }
