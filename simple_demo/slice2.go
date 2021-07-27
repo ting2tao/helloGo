1fef70e91aade6a37525008c0e6e10fb294cda0f
史纯涛 <1403528766@qq.com>
2021-07-27 17:57:22 +0800

    feat(new): add reptile

--- simple_demo/slice2.go
+++ simple_demo/slice2.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	//使用make 定义slice
@@ -13,9 +16,20 @@ func main() {
 	println(&a[2])
 	println(s2)
 	println(string(s2))
-	fmt.Println("元素数量：",len(s2),"容量：",cap(s2))
+	fmt.Println("元素数量：", len(s2), "容量：", cap(s2))
 
-	s3:=s2[1:3]
+	s3 := s2[1:3]
 	println(string(s3))
-	fmt.Println("元素数量：",len(s3),"容量：",cap(s3))
+	fmt.Println("元素数量：", len(s3), "容量：", cap(s3))
+
+	terminal := "h5" // 默认为提交的终端
+	viewRecordViewTerminal := "pc/h5"
+	if !strings.Contains(viewRecordViewTerminal, terminal) && viewRecordViewTerminal != "" {
+		terminal = fmt.Sprintf("%v/%v", viewRecordViewTerminal, terminal)
+	}
+	if strings.Contains(viewRecordViewTerminal, terminal) && viewRecordViewTerminal != "" {
+		terminal = viewRecordViewTerminal
+	}
+
+	fmt.Println(terminal)
 }
---
+++ try/getMovie.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	for i := 0; i < 10; i++ {
+		parseUrls("https://movie.douban.com/top250?start=" + strconv.Itoa(25*i))
+	}
+	elapsed := time.Since(start)
+	fmt.Printf("Took %s\n", elapsed)
+}
+
+func parseUrls(url string) {
+	body := fetch(url)
+	body = strings.Replace(body, "\n", "", -1)
+	rp := regexp.MustCompile(`<div class="hd">(.*?)</div>`)
+	titleRe := regexp.MustCompile(`<span class="title">(.*?)</span>`)
+	idRe := regexp.MustCompile(`<a href="https://movie.douban.com/subject/(\d+)/"`)
+	items := rp.FindAllStringSubmatch(body, -1)
+	for _, item := range items {
+		fmt.Println(idRe.FindStringSubmatch(item[1])[1],
+			titleRe.FindStringSubmatch(item[1])[1],
+			titleRe.FindStringSubmatch(item[1])[1])
+	}
+}
+
+func fetch(url string) string {
+	fmt.Println("Fetch Url", url)
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Http get err:", err)
+		return ""
+	}
+	if resp.StatusCode != 200 {
+		fmt.Println("Http status code:", resp.StatusCode)
+		return ""
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Read error", err)
+		return ""
+	}
+	return string(body)
+}
