c83b466107496cd536e624a607298792fea7ece1
shichuntao <1403528766@qq.com>
2023-08-04 15:39:51 +0800

    feat(multi-pm): 多生产者消费者

--- try/multi_productor_consumer.go
+++ try/multi_productor_consumer.go
@@ -2,26 +2,62 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func main() {
-	ch := make(chan int, 5)
+// 多生产者消费者
+var wg2 sync.WaitGroup
 
-	go product(15, ch)
-	close(ch)
-	for i := range ch {
-		fmt.Println(i)
+type PCD struct {
+	Name string
+	ID   int
+}
+
+func main() {
+	ch := make(chan *PCD, 2)
+
+	go product(20, ch)
+
+	for i := 0; i < 5; i++ {
+		wg2.Add(1)
+		go do(ch, &wg2)
+	}
+	wg2.Wait()
+}
+
+func do(ch chan *PCD, wg *sync.WaitGroup) error {
+	defer wg.Done()
+	//for i := range ch {
+	//	fmt.Println("消费", i)
+	//}
+	for {
+		select {
+		case i, ok := <-ch:
+			if !ok {
+				fmt.Println("channel is empty")
+				return nil
+			}
+			fmt.Println("消费", i.Name)
+		case <-time.After(time.Second * 5):
+			fmt.Println("timeout")
+			return nil
+
+		}
 	}
 
 }
 
-func product(n int, ch chan int) {
-	for i := 0; i < n; i++ {
-		if i%2 == 0 {
-			time.Sleep(10000)
+func product(n int, ch chan *PCD) {
+	for i := 1; i <= n; i++ {
+		if i%4 == 0 {
+			time.Sleep(time.Second * 2)
 		}
-		ch <- i
+		pcd := &PCD{
+			Name: fmt.Sprintf("Name%d", i),
+			ID:   i,
+		}
+		ch <- pcd
 	}
-
+	close(ch)
 }
